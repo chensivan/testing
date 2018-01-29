fa959cf257d079d27b5b12d3e4ccc634c9fb58d4
chensivan <chensivancy@gmail.com>
2018-01-28 22:03:54 -0800

    pilot version

--- crowdpage/testing.js
+++ crowdpage/testing.js
@@ -2,35 +2,48 @@ var app = angular.module("myTestApp", ["ngRoute"]);
 
 
 app.controller('myTestController',[ '$scope', '$http', function($scope, $http) {
-  var arry = [1,2,3,4,5,6]
+  var arry = []
+  $scope.answerObj = {}
+  for (var i = 1; i <= 45; i++) {
+     arry.push(i);
+     $scope.answerObj[i] = {
+       'startTime': [],
+       'leaveTime': [],
+       'isSelected': false
+     }
+  }
   $scope.videosSet = _.shuffle(arry);
   $scope.selectedVideos = []
   var socket = io.connect();
   socket.emit('start');
-  $scope.answerObj = {}
-  for(var i = 1; i <= 30; i++) {
-    $scope.answerObj[i] = {
-      'startTime': [],
-      'leaveTime': [],
-      'isSelected': false
-    }
-  }
 
   $scope.startPlayer = function(ind){
     angular.element(document).find('#'+ind).get(0).play()
-    $scope.answerObj[ind]['startTime'].push((new Date()).getTime())
+    if (ind != -1 && ind != -2) {
+      console.log('non-example');
+      $scope.answerObj[ind]['startTime'].push((new Date()).getTime())
+    }
   };
 
  $scope.pausePlayer = function(ind){
    angular.element(document).find('#'+ind).get(0).pause()
-   $scope.answerObj[ind]['leaveTime'].push((new Date()).getTime())
+   if (ind != -1 && ind != -2) {
+     console.log('non-example');
+     $scope.answerObj[ind]['leaveTime'].push((new Date()).getTime())
+   }
  }
 
  $scope.select = function(ind) {
    var elem = angular.element(document).find('#'+ind)
    if (elem.hasClass('selected')) elem.removeClass('selected')
    else elem.addClass('selected')
-   $scope.answerObj[ind]['isSelected'] = !$scope.answerObj[ind]['isSelected']
+   if (ind != -1 && ind != -2) {
+     console.log('non-example');
+     $scope.answerObj[ind]['isSelected'] = !$scope.answerObj[ind]['isSelected']
+   }
+ }
+ $scope.startTask = function() {
+   $scope.startTheTask = !$scope.startTheTask
  }
 
  $scope.submit = function(){
